5bc1ef67697a14af9b07dc82240fb79b61f50ccc
Aden Carver <99aden@gmail.com>
2020-09-02 18:30:52 -0400

    created end-point for contact creation

---
+++ login/API/create.php
@@ -0,0 +1,46 @@
+<?php
+
+  $inData = getRequestInfo();
+
+  $firstName = $inData["firstName"];
+  $lastName = $inData["lastName"];
+  $email = $inData["email"];
+  $number = $inData["number"];
+
+  //MUST SORT OUT ISSUE WITH CONNECTING TO DB
+  //ERROR: 403
+  //$conn = new myspli("localhost", "smallpro_aden", "smallproCOP4331", "smallpro_cop4331");
+
+  //establish connection
+  if($conn->connect_error) {
+    returnWithError($conn->connect_error);
+  }
+
+  else {
+    $sql = "insert into Contacts (FirstName, LastName, Email, PhoneNumber) VALUES '" . $firstName . "','" . $lastName . "','" . $email . "','" . $number . "')";
+
+    if($result = $conn->query($sql) != TRUE) {
+      returnWithError($conn->error);
+    }
+
+    $conn->close();
+  }
+
+  //returns with no error
+  returnWithError("");
+
+  //aux functs
+  function getRequestInfo() {
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj ) {
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+  function returnWithError( $err ) {
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
