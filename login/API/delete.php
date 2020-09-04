6e91020e6fa932618f491eea15b5c056f1b258fd
Aden Carver <99aden@gmail.com>
2020-09-04 19:24:19 -0400

    wrote script

--- login/API/delete.php
+++ login/API/delete.php
@@ -1,5 +1,56 @@
 <?php
 
+  $inData = getRequestInfo();
 
+  $firstName = $inData["firstName"];
+  $lastName = $inData["lastName"];
+  $email = $inData["email"];
+  $number = $inData["number"];
 
+  $conn = new myspli("localhost", "smallpro_cop4331", "Popgame1!", "smallpro_cop4331");
+
+  //establish connection
+  if($conn->connect_error) {
+    returnWithError($conn->connect_error);
+  }
+
+  else {
+    $sql = "DELETE FROM Contacts WHERE FirstName='" . $firstName . "' and LastName='" . $lastName . "' and Email='" . $email . "' and PhoneNumber='" . $number . "' and FK_UserID='" . $id . "'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+			$row = $result->fetch_assoc();
+			$firstName = $row["FirstName"];
+			$lastName = $row["LastName"];
+			$id = $row["UserID"];
+
+			returnWithInfo($firstName, $lastName, $id );
+		}
+
+		else {
+			returnWithError( "No Records Found" );
+		}
+
+    $conn->close();
+  }
+
+  //aux functs
+  function getRequestInfo() {
+    return json_decode(file_get_contents('php://input'), true);
+  }
+
+  function sendResultInfoAsJson( $obj ) {
+    header('Content-type: application/json');
+    echo $obj;
+  }
+
+  function returnWithError( $err ) {
+    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+  }
+
+  function returnWithInfo( $firstName, $lastName, $id, $email, $number) {
+    $retValue = '{"FK_UserID":' . $id . ',"FirstName":"' . $firstName . '","LastName":"' . $lastName .  '","Email":"' . $email . '","PhoneNumber":"' . $number . '","error":""}';
+    sendResultInfoAsJson( $retValue );
+  }
 ?>
