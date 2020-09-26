20aebe9b3647ddb1dc92280a3e807287195b6b68
adenrae <99aden@gmail.com>
2020-09-26 14:57:28 -0400

    added extra error handling

--- login/API/create.php
+++ login/API/create.php
@@ -6,7 +6,8 @@
   $lastName = $inData["lastName"];
   $email = $inData["email"];
   $number = $inData["number"];
-  $id = $inData["userId"];
+  $id = $inData["id"];
+
 
   $conn = new mysqli("localhost", "smallpro_cop4331", "Popgame1!", "smallpro_cop4331");
 
@@ -16,17 +17,33 @@
   }
 
   else {
-    $sql = "insert into `Contacts`(`FirstName`, `LastName`, `Email`, `PhoneNumber`, `FK_UserID`) VALUES '" . $firstName . "','" . $lastName . "','" . $email . "','" . $number . "','" . $id . "')";
-
+    if(strlen($firstName) == 0)
+        returnWithError( "First name missing" );
+    
+    if(strlen($lastName) == 0)
+        returnWithError( "Last name missing" );
+        
+    if(strlen($email) == 0 && strlen($number) == 0)
+        returnWithError( " Both contact fields empty "); 
+    
+    $sql = "INSERT INTO `Contacts`(`FirstName`, `LastName`, `Email`, `PhoneNumber`, `FK_UserID`) VALUES ('" . $firstName . "','" . $lastName . "','" . $email . "','" . $number . "'," . $id . ")";
+    //returnWithError($sql);
+   // $result = $conn->query($sql);
+    
     if($result = $conn->query($sql) != TRUE) {
-      returnWithError($conn->error);
+        returnWithError($conn->error);
     }
-
-    $conn->close();
+    
+     //echo "Contact successfully inserted";
+    else {
+        returnWithInfo($firstName, $lastName, $email, $number);
+    }
+	
+	$conn->close();
   }
 
   //returns with no error
-  returnWithError("");
+  //returnWithError("");
 
   //aux functs
   function getRequestInfo() {
@@ -40,6 +57,11 @@
 
   function returnWithError( $err ) {
 		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+ function returnWithInfo( $firstName, $lastName, $email, $number ) {
+		$retValue = '{"firstName":"' . $firstName . '","lastName":"' . $lastName . '","email":"' . $email . '","number":"' . $number . '","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
 ?>
