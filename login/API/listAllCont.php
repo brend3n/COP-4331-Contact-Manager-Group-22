fedbf9a92ed44e443388995a8845d8ad9d2afe5e
adenrae <99aden@gmail.com>
2020-09-26 15:00:13 -0400

    lists all contacts given userId

---
+++ login/API/listAllCont.php
@@ -0,0 +1,64 @@
+<?php
+
+    $inData = getRequestInfo();
+
+    $id = $inData["id"];
+    
+    $conn = new mysqli("localhost", "smallpro_cop4331", "Popgame1!", "smallpro_cop4331");
+
+    //establishing connection
+    if($conn->connect_error) {
+        returnWithError($conn->connect_error);
+    }
+    
+    else {
+        $sql = "SELECT * FROM `Contacts` WHERE FK_UserID=" . $id;
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0) {
+            //number of search results
+            $numResults = $result->num_rows;
+
+            $results = array();
+
+            while($row = $result->fetch_assoc()) {
+                $results[] = array(
+                "firstName" => $row["FirstName"],
+                "lastName" => $row["LastName"],
+                "email" => $row["Email"],
+                "number" => $row["PhoneNumber"]
+                );
+            }
+
+            //return json_encode($results);
+            returnWithInfo($results);
+        }
+
+        else {
+            returnWithError( "No Records Found");
+        }
+
+        $conn->close();
+    }
+    
+    //aux functs
+    function getRequestInfo() {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+    
+    function sendResultInfoAsJson( $obj ) {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+    
+    function returnWithError( $err ) {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+    
+    function returnWithInfo( $results) {
+        $retValue = '{"results":' . json_encode($results) . ',"error":""}';
+        sendResultInfoAsJson( $retValue );
+    }
+        
+?>
