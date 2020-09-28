a1067a56560b6dc1ce832f38eea33041b772f51d
JainavPatel <54753556+JainavPatel@users.noreply.github.com>
2020-09-28 01:14:14 -0400

    Add files via upload
    Fixed formatting issues and search and partial search on code js file Fixed error messages on read php and imported new list to hand allcontacts on newLogin html file

---
+++ login/Front-End/read (3).php
@@ -0,0 +1,75 @@
+
+<?php
+
+  $inData = getRequestInfo();
+
+  $id = $inData["id"];
+  $search = $inData["search"];
+
+  $conn = new mysqli("localhost", "smallpro_cop4331", "Popgame1!", "smallpro_cop4331");
+
+  //establishing connection
+  if($conn->connect_error) {
+    returnWithError($conn->connect_error);
+  }
+
+  else {
+    if(empty($search))
+    {
+       // returnWithError(" Search string required ");
+        echo "Need input something in search!";
+        $conn->close();
+    }
+    
+    $sql = "SELECT * FROM `Contacts` WHERE FK_UserID=" . $id . " AND (FirstName LIKE '%" . $search . "%' OR LastName LIKE '%" . $search . "%' OR Email LIKE '%" . $search . "%' OR PhoneNumber LIKE '%" . $search . "%')";
+    $result = $conn->query($sql);
+
+    if($result->num_rows > 0) {
+      //number of search results
+      $numResults = $result->num_rows;
+
+      $results = array();
+
+      while($row = $result->fetch_assoc()) {
+        $results[] = array(
+          "firstName" => $row["FirstName"],
+          "lastName" => $row["LastName"],
+          "email" => $row["Email"],
+          "number" => $row["PhoneNumber"]
+        );
+        
+        echo "First Name: " . $row["FirstName"] . " | Last Name: " . $row["LastName"] . " | Email: " . $row["Email"] . " | Phone Number: " . $row["PhoneNumber"] . "lineEnded";
+      }
+
+      //return json_encode($results);
+      //returnWithInfo($results);
+    }
+
+    else {
+      returnWithError( "No Records Found");
+    }
+
+    $conn->close();
+  }
+
+  //aux functs
+  function getRequestInfo() {
+    return json_decode(file_get_contents('php://input'), true);
+  }
+
+  function sendResultInfoAsJson( $obj ) {
+    header('Content-type: application/json');
+    echo $obj;
+  }
+
+  function returnWithError( $err ) {
+    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+  }
+
+  function returnWithInfo( $results) {
+    $retValue = '{"results":' . json_encode($results) . ',"error":""}';
+    sendResultInfoAsJson( $retValue );
+  }
+
+?>
